baab661785b05bd46fe227723f00bfc8fe270a24
Sesa Adit <sesadit@gmail.com>
2015-04-14 22:35:04 +0800

    Update 3 files:
    	115.md
    	115.html
    	md2html.pl

--- md2html.pl
+++ md2html.pl
@@ -5,11 +5,14 @@ while(<>) {
 	chomp;
 	#s{(?:<\()(https?://[^\s]+)}{($1)}g;
 	#s{(https?://[^\s]+)}{[$1]($1)}g;
-	s{(?<!\]\()(https?://[^\s]+)}{[$1]($1)}g;
+	#s{(?<!\]\()(https?://[^\s]+)}{[$1]($1)}g;
+	s{(https?://[^\s]+)}{<a href="$1">$1</a>}g;
 	push @text,$_;
 }
 
-use Text::Markdown qw/markdown/;
-print markdown(join("\n",@text));
+#use Text::Markdown qw/markdown/;
+#print markdown(join("\n",@text));
+
+print join("<BR/>\n",@text);
 
 
