2a62023c45e53cc8d2ac6b2d580b72a570f88bf4
Sesa Adit <sesadit@gmail.com>
2018-07-26 02:55:20 +0800

    Add generator script: make.pl

---
+++ make.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+use strict;
+print STDERR "Generating <opensearch.html> ...\n";
+
+
+my $di;
+my @files;
+opendir $di,"opensearch" or die("$!\n");
+foreach(readdir $di) {
+	push @files,$_ if(m/\.xml$/i);
+}
+close $di;
+
+my $fo;
+open $fo,'>','opensearch.html' or die("$!\n");
+
+print $fo '<head>',"\n";
+foreach(@files) {
+	my $n = $_;
+	$n =~ s/^.*\///;
+	$n =~ s/\.xml$//i;
+	print STDERR "    $n ...\n";
+	print $fo '<link rel="search" type="application/opensearchdescription+xml"',"\n";
+	print $fo "    title=\"$n search\"\n";
+	print $fo "    href=\"https://boqi.github.io/opensearch/$_\"\n";
+	print $fo ">\n";
+}
+print $fo '</head>',"\n";
+print $fo "<html>\n<body>\n<ul>\n";
+foreach(@files) {
+	print $fo "    <li>https://boqi.github.io/opensearch/$_</br></li>\n";
+}
+print $fo "</ul>\n";
+print $fo '<span style="text-align:right">Last Update ' . scalar(localtime) . "</span>\n";
+print $fo "</body>\n</html>\n";
+
+close $fo;
+
+print STDERR "[OK]\n";
+
+exit 0;
